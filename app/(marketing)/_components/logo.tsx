8126b6a6d27aece75e4a6929ddd9663a1f56d002
Vanshil Patel <vanshilpatel12@gmail.com>
2024-01-26 17:27:01 +0530

    Landing page setup

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,23 @@
+import {Poppins} from "next/font/google";
+import Image from "@/node_modules/next/image";
+import {cn} from "@/lib/utils"
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["400","600"]
+});
+
+const Logo = () => {
+    return ( 
+        <div className="hidden md:flex items-center gap-x-2">
+            <Image
+            src="/logo.svg"
+            height="40"
+            width="40"
+            alt="Logo"/>
+            <p className={cn("font-semibold", font.className)}>Jotion</p>
+        </div>
+     );
+}
+ 
+export default Logo;
