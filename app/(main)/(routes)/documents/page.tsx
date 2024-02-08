c99e68cf58fe010770564a3bf4564cbed2d68885
Vanshil Patel <vanshilpatel12@gmail.com>
2024-02-08 23:34:10 +0530

    Documents page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,16 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import Image from "@/node_modules/next/image";
+import {useUser} from "@clerk/clerk-react";
+import {PlusCircle} from "lucide-react";
+
 
 
 const DocumentsPage = () => {
+
+    const {user} = useUser()
+
     return ( 
         <div className="h-full flex flex-col items-center justify-center space-y-4">
         <Image
@@ -20,6 +27,12 @@ const DocumentsPage = () => {
           alt="Empty"
           className="hidden dark:block"
         />
+        <h2 className="text-lg font-medium">Welcome to {user?.firstName}&apos; Jotion</h2>
+        <Button>
+            <PlusCircle className= "h-4 w-4 mr-2"/>
+            Create a note 
+        </Button>
+
         </div>
      );
 }
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,8 +4,10 @@ import { cn } from "@/lib/utils"
 import { usePathname } from "@/node_modules/next/navigation"
 import {ChevronsLeft, MenuIcon} from "lucide-react"
 import { ElementRef, useRef, useState } from "react"
+import { UserItem } from "./user-item"
 
-import {useMediaQuery} from "usehooks-ts"
+import {useMediaQuery} from "usehooks-ts";
+
 
 
 export const Navigation = () => {
@@ -30,21 +32,44 @@ export const Navigation = () => {
     }
 
 
-    const handleMouseUp = (e:MouseEvent) => {
-        if(!isResizingRef.current) return;
-
+    const handleMouseUp = () => {
+        isResizingRef.current = false;
+        document.removeEventListener("mousemove", handleMouseMove);
+        document.removeEventListener("mouseup", handleMouseUp);
+      };
+    
+    
+      const handleMouseMove = (event: MouseEvent) => {
+        if (!isResizingRef.current) return;
         let newWidth = event.clientX;
-
-        if(newWidth < 240) newWidth = 240;
-        if(newWidth > 480) newWidth = 480;
-
-        
-    }
     
+        if (newWidth < 240) newWidth = 240;
+        if (newWidth > 480) newWidth = 480;
     
-    const handleMouseMove = () => {
+        if (sidebarRef.current && navbarRef.current) {
+          sidebarRef.current.style.width = `${newWidth}px`;
+          navbarRef.current.style.setProperty("left", `${newWidth}px`);
+          navbarRef.current.style.setProperty("width", `calc(100% - ${newWidth}px)`);
+        }
+      };
 
-    }
+    const resetWidth = () => {
+        if (sidebarRef.current && navbarRef.current) {
+          setIsCollapsed(false);
+          setIsResetting(true);
+    
+          sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+          navbarRef.current.style.setProperty(
+            "width",
+            isMobile ? "0" : "calc(100% - 240px)"
+          );
+          navbarRef.current.style.setProperty(
+            "left",
+            isMobile ? "100%" : "240px"
+          );
+          setTimeout(() => setIsResetting(false), 300);
+        }
+      };
 
 
     return (
@@ -60,8 +85,8 @@ export const Navigation = () => {
         isMobile && "opacity-100")}>
             <ChevronsLeft className="h-6 w-6" />
         </div>
-            <div>
-            <p>Action Items</p>
+        <div>
+           <UserItem />
         </div>
         <div className="mt-4">
             <p>Documents</p>
